b0cd1b7eb8aeb0b02056389f79539907709acf7e
OnikenX <OnikenX@pm.me>
2021-04-21 11:20:56 +0100

    projects files setuped

---
+++ game-engine-impl.md
@@ -0,0 +1,14 @@
+# Twitter API acess
+
+The **twitter api needs an user to login** to its twitter user account, and give an pin back, **that work needs to be done in the client(game) side**, I can try to implement it in the server side given the user and password, **but it is probably against the twitter's TOS**. 
+
+
+## Implementation
+
+### Server side
+
+The server is the app, it will ask for the user pin and so it will log him in in the app.
+
+### Client side
+
+The client must receive the messange deencrypt it to send to the game engine.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use crate::ac7ion::Ac7ion;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -7,17 +9,80 @@ mod tests {
 
     #[test]
     fn test_twitter_api() {
-        use dotenv::dotenv;
-        use std::env;
-        dotenv().ok();
-      
-        for (key, value) in env::vars() {
-            println!("{}: {}", key, value);
-        }
-        let secret = env::vars()["secret_key"]
+        //IMPORTANT: make an app for yourself at apps.twitter.com and get your
+        //key/secret into these files; these examples won't work without them
+        let action = Ac7ion::generic_init();
     }
 }
 
+mod ac7ion {
+    //! Lib for AC7ION project
+    use async_std::sync::Arc;
+    use egg_mode::KeyPair;
 
-mod AC7ION {
+    pub struct Ac7ion {
+        pub token: egg_mode::KeyPair,
+        pub clients : Arc<Vec<a7_client::A7Client>>,
+    }
+    mod a7_client {
+        pub struct A7Client {
+            pub token : &egg_mode::Token,
+            pub user_id : u64,
+            pub screen_name : String,
+        }
+    }
+
+    impl Ac7ion {
+
+        /// Gets the consumer_key and consumer_secret contents in files
+        /// with the same name in the path, and creates a new Ac7tion with ```Ac7tion::new()```
+        ///
+        /// # Panic
+        /// Panics if an error occurs with the authentication
+        pub async fn generic_init() -> Ac7ion{
+            let consumer_key = include_str!("consumer_key").trim();
+            let consumer_secret = include_str!("consumer_secret").trim();
+            Ac7ion::new(String::from(consumer_key), String::from(consumer_secret)).await.unwrap()
+        }
+
+
+        /// The initializer for the Ac7ion struct
+        ///
+        /// It verifies the keys given and returns the struct if the
+        /// verification of the app went well
+        ///
+        /// # Arguments
+        /// Receives an public and secret key, those are given in the app dashboard
+        ///
+        /// # Error
+        /// It returns the error given by the app authenticator
+        pub async fn new(public_key: String, secret_key: String) -> Result<Ac7ion, egg_mode::error::Error> {
+            let con_token = egg_mode::KeyPair::new(public_key.clone(), secret_key.clone());
+            // "oob" is needed for PIN-based auth; see docs for `request_token` for more info
+            match egg_mode::auth::request_token(&con_token, "oob").await{
+                Ok(token) =>{
+                    Ok(Ac7ion { token, clients: Arc::new(Vec::new())})
+                }
+                Err(e) => {
+                    Err(e)
+                }
+            }
+        }
+
+        /// Adds a new client to the list
+        pub async fn add_client(&self, pin: i64){
+
+        }
+
+        pub async fn string_to_keypair(key :String, secret : String) -> KeyPair {
+            egg_mode::KeyPair::new(key, secret)
+        }
+
+        pub async fn connect_client(&self, username: String, user_id : u64, acess_token : KeyPair){
+            let token = egg_mode::Token::Access{
+                consumer: self.token,
+                access: acess_token
+            }
+        }
+    }
 }
